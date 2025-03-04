851a05db784b8a59ceb6a5ccac3b2771d9c062ef
Tomasz Nowakowski <toalak@gmail.com>
2025-03-04 11:15:14 +0100

    info about gold on the right panel done, granting rewards wip

--- fantasy_console/game/event_combat.lua
+++ fantasy_console/game/event_combat.lua
@@ -1,3 +1,6 @@
+local dice = require "game/dice"
+
+
 local map = require "game/map"
 
 
@@ -26,6 +29,43 @@ function event.generate_travel_options()
         local s = "Go to " .. number .. " room [" .. room .. "]"
         table.insert(event.options, s)
     end
+end
+
+function event.grant_rewards()
+    local gold = math.random(5, 20) * Base_difficulty
+    local s = "You looted " .. gold .. "GP"
+    local dices = {}
+    local chance = math.random(101)
+    local amount = 0
+    local reds = 0
+    local golds = 0
+    if chance <= 50 then
+        amount = 1
+    elseif chance <= 80 then
+        amount = 2
+    elseif chance <= 98 then
+        amount = 3
+    else
+        amount = 4
+    end
+    for i = 1, amount do
+        local die_type = dice.red
+        reds = reds + 1
+        if math.random(101) <= 30 then
+            die_type = dice.gold
+            reds = reds - 1
+            golds = golds + 1
+        end
+        table.insert(dices, die_type)
+    end
+    if reds > 0 then
+        s = s .. ", " .. reds .. "x red die"
+    end
+    if golds > 1 then
+        s = s .. ", " .. golds .. "x gold die"
+    end
+    s = s .. "."
+    return s
 end
 
 return event
