3ed8dc4615d41f85970c0a482ac17575fa36c21f
Tomasz Nowakowski <toalak@gmail.com>
2025-03-05 16:32:28 +0100

    I made the rolls twice as fast

--- fantasy_console/_cart.lua
+++ fantasy_console/_cart.lua
@@ -95,7 +95,7 @@ end
 
 function Update()
     F = F + 1
-    if State == states.rolling and F % 10 == 0 then
+    if State == states.rolling and F % 5 == 0 then
         if Current_side + 1 <= #Rolls[1][2] then
             Current_side = Current_side + 1
             dice.update_last_results(Rolls, Current_side)
