b22058d3de29c8807b5afb0492cf702d2e11cd38
Tomasz Nowakowski <toalak@gmail.com>
2025-03-05 17:55:57 +0100

    Two possible events described; not handled yet

--- fantasy_console/game/event_random.lua
+++ fantasy_console/game/event_random.lua
@@ -6,6 +6,28 @@ event.header = "This is random event."
 event.options = {"Proceed"}
 event.base_options = {"Proceed"}
 
+
+event.events = {}
+
+--event.infighting = {}
+--event.header = "You see two groups fighting\neach other.\nWhat do you do?"
+--event.options = {
+--    "Help the smaller group",
+--    "Help the larger group"
+--}
+
+
+event.free_meal = {}
+event.free_meal.header = "You see an iron ration on table."
+event.free_meal.options = {"Yum!"}
+table.insert(event.events, event.free_meal)
+
+event.decrease_danger = {}
+event.decrease_danger.header = "You see a poster with your likeness\nhanged on the wall."
+event.decrease_danger.options = {"Tear down the poster!"}
+table.insert(event.events, event.decrease_danger)
+
+
 function event.generate_travel_options()
     event.options = {}
     for i, room in ipairs(map.doors_to) do
